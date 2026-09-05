1133d166ede5e187024acda7b8566ce734792fa2
MASONG\mason <masondev@gmail.com>
2026-09-05 23:47:52 +1000

    for deploy

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from src.server.api import app as api_app
 app: FastAPI = gr.mount_gradio_app(api_app, demo, path="/")
 
 
-if __name__ == "__main__":
-	uvicorn.run(
-		app,
-		host="0.0.0.0",
-		port=int(os.getenv("PORT", "7860")),
-	)
+# if __name__ == "__main__":
+# 	uvicorn.run(
+# 		app,
+# 		host="0.0.0.0",
+# 		port=int(os.getenv("PORT", "7860")),
+# 	)
